006c3acbdf36d20150157f63c8370f8dfc75e653
Nathan Fredericks <nathan@nathfreder.dev>
2023-03-24 16:32:53 -0300

    Upload website

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script defer data-domain="mcpe.fun" src="https://analytics.mcpe.fun/js/script.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
